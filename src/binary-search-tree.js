f230e1fad6c4c726f8f8f86209f40234d238c3bd
ElizavetaRazumenko <razumenko99@mail.ru>
2023-04-17 22:02:21 +0300

    some solves

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,42 +6,179 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+class Node {
+  constructor(data) {
+    this.value = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 class BinarySearchTree {
 
+  constructor() {
+    this._root = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return {
+      data: this._root,
+    }
+    //throw new NotImplementedError('Not implemented');
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    //throw new NotImplementedError('Not implemented');
+    function addNewElement(elem, data) {
+
+      if (!elem) {
+        return new Node(data)
+      }
+  
+      if (elem.data === data) {
+        return elem;
+      }
+  
+      if (data < elem.data) {
+        elem.left = addNewElement(elem.left, data);
+      }
+
+      if (data > elem.value) {
+        elem.right = addNewElement(elem.right, data);
+      } return elem;
+
+    }
+
+    this._root = addNewElement(this._root, data)
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  has(data) {
+    //throw new NotImplementedError('Not implemented');
+    function hasEmement(elem, data) {
+      if (!elem) {
+        return false;
+      }
+
+      if (elem.data === data) {
+        return true;
+      }
+      if (data < elem.data) {
+        return hasEmement(elem.left, data);
+      } else {
+        return hasEmement(elem.right, data);
+      }
+    }
+    return hasEmement(this._root, data);
+
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  find(data) {
+    //throw new NotImplementedError('Not implemented');
+    function findElement(elem, data) {
+      if (!elem) {
+        return false;
+      }
+      if (elem.data == data) {
+        return elem;
+      }
+
+      if (data < elem.data) {
+        elem.left = findElement(elem.left, data); 
+      } else  {
+        elem.right = findElement(elem.right, data) 
+      }
+      
+    } 
+
+    this._root = findElement(this._root, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    //throw new NotImplementedError('Not implemented');
+    function removeElement(elem, data) {
+      if (!elem) {
+        return null;
+      }
+
+      if (data < elem.data) {
+        elem.left = removeElement(elem.left, data);
+        return elem;
+      } else if (elem.data < data) {
+        elem.right = removeElement(elem.right, data);
+        return elem;
+      } else {
+        if (!elem.left && !elem.right) {
+          return null;
+        } 
+
+        if (!elem.left) {
+          elem = elem.right;
+          return elem;
+        }
+
+
+        if (!elem.right) {
+          elem = elem.left;
+          return elem;
+        }
+
+        let minElementRight = elem.right;
+
+          while (minElementRight.left) {
+            minElementRight = minElementRight.left;
+          }
+
+          elem.data = minElementRight.value;
+
+          elem.right = removeElement(elem.right, minElementRight.data);
+
+          return elem;
+        }
+      }
+
+
+      this._root = removeElement(this._root, data);
+
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    //throw new NotImplementedError('Not implemented');
+    if (!this._root) {
+
+      return null;
+     }
+  
+        let elem = this._root;
+  
+  
+        while (elem.left) {
+          elem = elem.left;
+        }
+  
+      return elem.value;
+    }
+  
+  
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    //throw new NotImplementedError('Not implemented');
+    if (!this._root) {
+
+      return null;
+
+    }
+
+    let elem = this._root;
+    while (elem.right) {
+      elem = elem.right;
+    }
+
+    return elem.value;
   }
+
+  
 }
 
 module.exports = {
